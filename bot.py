76e0574e951e1a3e13b16b5a2eb1d36bbabc2ec5
robinescaffre <robin.escaffre@edu.esiee.fr>
2024-02-27 12:59:30 +0100

    Neo4j Vector Store

--- bot.py
+++ bot.py
@@ -1,35 +1,30 @@
 import streamlit as st
 from utils import write_message
+from llm import llm, embeddings
+from graph import graph
+from agent import generate_response
 
 # tag::setup[]
 # Page Config
-st.set_page_config("Ebert", page_icon=":movie_camera:")
+st.set_page_config("UR MOM", page_icon="😎")
 # end::setup[]
 
 # tag::session[]
 # Set up Session State
 if "messages" not in st.session_state:
     st.session_state.messages = [
-        {"role": "assistant", "content": "Hi, I'm the GraphAcademy Chatbot!  How can I help you?"},
+        {"role": "assistant", "content": "Yo bitch, what do you fuckin want"},
     ]
 # end::session[]
 
 # tag::submit[]
 # Submit handler
 def handle_submit(message):
-    """
-    Submit handler:
-
-    You will modify this method to talk with an LLM and provide
-    context using data from Neo4j.
-    """
-
     # Handle the response
     with st.spinner('Thinking...'):
-        # # TODO: Replace this with a call to your LLM
-        from time import sleep
-        sleep(1)
-        write_message('assistant', message)
+
+        response = generate_response(message)
+        write_message('assistant', response)
 # end::submit[]
 
 
